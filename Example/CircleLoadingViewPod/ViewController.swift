40a03e73788aae7f26e6884468bef49a04ae8324
frostmarchesan <55881881+frostmarchesan@users.noreply.github.com>
2021-11-24 14:51:15 -0300

    Initial commit

---
+++ Example/CircleLoadingViewPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CircleLoadingViewPod
+//
+//  Created by frostmarchesan on 11/24/2021.
+//  Copyright (c) 2021 frostmarchesan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
